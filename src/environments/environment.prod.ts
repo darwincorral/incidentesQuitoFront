9d560413d06543f06be6af5fbcc90e99a3defdc9
Darwin C. Narváez <35180908+darwincorral@users.noreply.github.com>
2022-06-20 12:29:49 -0500

    Modificacion de Url's

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   //APIS
-  URL_SERVICIOS_SEGURIDAD : '',
-  URL_SERVICIOS_CORREO : '',
+  URL_SERVICIOS_SEGURIDAD : 'http://servicios.amt.gob.ec:10664/micro_seguridades/usuario',
+  URL_SERVICIOS_CORREO : 'http://186.47.204.233:8080/micro_sendMail-0.0.1/mail/sendBasic',
+  URL_SERVICIOS : 'https://incidentes-amt.herokuapp.com',
+  URL_SERVICIOS_AMT:'http://servicios.amt.gob.ec:10664/micro_seguridades/personal',
 
   //ID DE APLICACION DE LA BD
   idAplicacion: '41',
